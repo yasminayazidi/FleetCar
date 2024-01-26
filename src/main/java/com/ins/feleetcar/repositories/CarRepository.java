b69da9693a27004f796f97aa623ea8bab94bc566
yasminayazidi <152389294+yasminayazidi@users.noreply.github.com>
2024-01-26 10:08:26 +0100

    Delete src directory

--- src/main/java/com/ins/feleetcar/repositories/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ins.feleetcar.repositories;
-
-import com.ins.feleetcar.entities.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car, Integer> {
-}
--- src/main/java/com/ins/feleetcar/repositories/DriverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ins.feleetcar.repositories;
-
-import com.ins.feleetcar.entities.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DriverRepository extends JpaRepository<Driver,Integer> {
-}
