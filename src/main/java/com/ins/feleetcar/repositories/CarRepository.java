e84536449cc348f81efb2687fd9160f1a640348c
yasmina.yazidi <y.yazidi@inssolution.net>
2024-01-29 11:35:39 +0100

    car

--- src/main/java/com/ins/feleetcar/repositories/CarRepository.java
+++ src/main/java/com/ins/feleetcar/repositories/CarRepository.java
@@ -2,8 +2,9 @@ package com.ins.feleetcar.repositories;
 
 import com.ins.feleetcar.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource
 public interface CarRepository extends JpaRepository<Car, Integer> {
 }
