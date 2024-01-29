ba1f8002b4f53d61d11cbf53622f25e76a63a662
yasmina.yazidi <y.yazidi@inssolution.net>
2024-01-29 11:13:41 +0100

    delete

--- src/main/java/com/ins/feleetcar/repositories/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ins.feleetcar.repositories;
-
-import com.ins.feleetcar.entities.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin,Integer> {
-}
